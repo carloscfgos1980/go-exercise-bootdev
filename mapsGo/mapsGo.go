cd6e2798630fb48d2fcb89dd9b48cce3b77db3c6
carlosinfante <kuva5008@gmail.com>
2026-08-19 21:24:36 +0200

    mutations

--- mapsGo/mapsGo.go
+++ mapsGo/mapsGo.go
@@ -21,3 +21,23 @@ type user struct {
 	name        string
 	phoneNumber int
 }
+
+// Mutations Maps in Go
+
+func DeleteIfNecessary(users map[string]user2, name string) (deleted bool, err error) {
+	u, ok := users[name]
+	if !ok {
+		return false, errors.New("not found")
+	}
+	if !u.scheduledForDeletion {
+		return false, nil
+	}
+	delete(users, name)
+	return true, nil
+}
+
+type user2 struct {
+	name                 string
+	number               int
+	scheduledForDeletion bool
+}
--- mapsGo/mapsGo_test.go
+++ mapsGo/mapsGo_test.go
@@ -63,3 +63,62 @@ func TestGetUserMap(t *testing.T) {
 		}
 	}
 }
+
+func equalUser2Maps(a, b map[string]user2) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for k, v := range a {
+		if vb, ok := b[k]; !ok || vb != v {
+			return false
+		}
+	}
+	return true
+}
+func TestDeleteIfNecessary(t *testing.T) {
+	tests := []struct {
+		users             map[string]user2
+		name              string
+		expectedErrString string
+		expectedUsers     map[string]user2
+		expectedDeleted   bool
+	}{
+		{
+			map[string]user2{"Erwin": {"Erwin", 14355550987, true}, "Levi": {"Levi", 98765550987, true}, "Hanji": {"Hanji", 18265554567, true}},
+			"Erwin",
+			"",
+			map[string]user2{"Levi": {"Levi", 98765550987, true}, "Hanji": {"Hanji", 18265554567, true}},
+			true,
+		},
+		{
+			map[string]user2{"Erwin": {"Erwin", 14355550987, false}, "Levi": {"Levi", 98765550987, false}, "Hanji": {"Hanji", 18265554567, false}},
+			"Erwin",
+			"",
+			map[string]user2{"Erwin": {"Erwin", 14355550987, false}, "Levi": {"Levi", 98765550987, false}, "Hanji": {"Hanji", 18265554567, false}},
+			false,
+		},
+		{
+			map[string]user2{"Erwin": {"Erwin", 14355550987, true}, "Levi": {"Levi", 98765550987, true}, "Hanji": {"Hanji", 18265554567, true}},
+			"Eren",
+			"not found",
+			map[string]user2{"Erwin": {"Erwin", 14355550987, true}, "Levi": {"Levi", 98765550987, true}, "Hanji": {"Hanji", 18265554567, true}},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		deleted, err := DeleteIfNecessary(tt.users, tt.name)
+		fmt.Printf("DeleteIfNecessary(%v, %v) = %v, %v; want %v, %v\n", tt.users, tt.name, deleted, err, tt.expectedDeleted, tt.expectedErrString)
+		if err != nil {
+			if err.Error() != tt.expectedErrString {
+				t.Errorf("DeleteIfNecessary(%v, %v) returned unexpected error: got %v, want %v", tt.users, tt.name, err.Error(), tt.expectedErrString)
+			}
+			continue
+		}
+		if deleted != tt.expectedDeleted {
+			t.Errorf("DeleteIfNecessary(%v, %v) = %v; want %v", tt.users, tt.name, deleted, tt.expectedDeleted)
+		}
+		if !equalUser2Maps(tt.users, tt.expectedUsers) {
+			t.Errorf("DeleteIfNecessary(%v, %v) modified users map incorrectly: got %v; want %v", tt.users, tt.name, tt.users, tt.expectedUsers)
+		}
+	}
+}
