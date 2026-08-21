128645d873381e4503b09f52eaef47a3ce8a74c8
carlosinfante <kuva5008@gmail.com>
2026-08-21 13:20:52 +0200

    nil pointers

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -4,8 +4,42 @@ import (
 	"strings"
 )
 
+// Pointers dereference the value of a variable by using the * operator. This allows you to modify the value of a variable directly, rather than working with a copy of the value. In this example, we use pointers to remove profanity from a message string by modifying the original string in place.
 func RemoveProfanity(message *string) {
 	*message = strings.ReplaceAll(*message, "fubb", "****")
 	*message = strings.ReplaceAll(*message, "shiz", "****")
 	*message = strings.ReplaceAll(*message, "witch", "*****")
+}
+
+// Pay by reference allows you to pass a pointer to a variable instead of a copy of the variable's value. This can be more efficient for large data structures, as it avoids the overhead of copying the data. In this example, we use pointers to modify the original Analytics struct in place, rather than working with a copy of the struct.
+type Analytics struct {
+	MessagesTotal     int
+	MessagesFailed    int
+	MessagesSucceeded int
+}
+
+type Message struct {
+	Recipient string
+	Success   bool
+}
+
+func AnalyzeMessage(a *Analytics, msg Message) {
+	if msg.Success {
+		a.MessagesSucceeded++
+	} else {
+		a.MessagesFailed++
+	}
+	a.MessagesTotal++
+}
+
+// Nil pointers
+func RemoveProfanitySafe(message *string) {
+	if message == nil {
+		return
+	}
+	messageVal := *message
+	messageVal = strings.ReplaceAll(messageVal, "fubb", "****")
+	messageVal = strings.ReplaceAll(messageVal, "shiz", "****")
+	messageVal = strings.ReplaceAll(messageVal, "witch", "*****")
+	*message = messageVal
 }
--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -33,3 +33,71 @@ func TestRemoveProfanity(t *testing.T) {
 		})
 	}
 }
+
+func TestAnalyzeMessage(t *testing.T) {
+	tests := []struct {
+		initialAnalytics Analytics
+		newMessage       Message
+		expected         Analytics
+	}{
+		{
+			initialAnalytics: Analytics{MessagesTotal: 0, MessagesFailed: 0, MessagesSucceeded: 0},
+			newMessage:       Message{Recipient: "mickey", Success: true},
+			expected:         Analytics{MessagesTotal: 1, MessagesFailed: 0, MessagesSucceeded: 1},
+		},
+		{
+			initialAnalytics: Analytics{MessagesTotal: 1, MessagesFailed: 0, MessagesSucceeded: 1},
+			newMessage:       Message{Recipient: "minnie", Success: false},
+			expected:         Analytics{MessagesTotal: 2, MessagesFailed: 1, MessagesSucceeded: 1},
+		},
+		{
+			initialAnalytics: Analytics{MessagesTotal: 2, MessagesFailed: 1, MessagesSucceeded: 1},
+			newMessage:       Message{Recipient: "goofy", Success: false},
+			expected:         Analytics{MessagesTotal: 3, MessagesFailed: 2, MessagesSucceeded: 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("initial: %+v, new: %+v", tt.initialAnalytics, tt.newMessage), func(t *testing.T) {
+			AnalyzeMessage(&tt.initialAnalytics, tt.newMessage)
+			if tt.initialAnalytics != tt.expected {
+				t.Errorf("expected %+v, got %+v", tt.expected, tt.initialAnalytics)
+			}
+		})
+	}
+}
+
+func TestRemoveProfanitySafe(t *testing.T) {
+	s1 := "English, motherfubber, do you speak it?"
+	s2 := "English, mother****er, do you speak it?"
+	s3 := "Does he look like a witch?"
+	s4 := "Does he look like a *****?"
+	testCases := []struct {
+		messageIn *string
+		expected  *string
+	}{
+		{
+			&s1,
+			&s2,
+		},
+		{
+			nil,
+			nil,
+		},
+		{
+			&s3,
+			&s4,
+		},
+		{
+			nil,
+			nil,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("messageIn: %v", tc.messageIn), func(t *testing.T) {
+			RemoveProfanitySafe(tc.messageIn)
+			if tc.messageIn != nil && tc.expected != nil && *tc.messageIn != *tc.expected {
+				t.Errorf("expected %q, got %q", *tc.expected, *tc.messageIn)
+			}
+		})
+	}
+}
