b750bc07bab9eddfac12b729c78b8fe043ecffd1
carlosinfante <kuva5008@gmail.com>
2026-08-23 11:19:41 +0200

    channels

--- channels/channels.go
+++ channels/channels.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// Concurrency is a powerful feature in Go that allows you to run multiple tasks simultaneously. In this example, we will demonstrate how to use goroutines and channels to send and receive messages concurrently.
 func sendEmail(message string) {
 	go func() {
 		time.Sleep(time.Millisecond * 250)
@@ -13,10 +14,38 @@ func sendEmail(message string) {
 	fmt.Printf("Email sent: '%s'\n", message)
 }
 
-// Don't touch below this line
-
 func Test(message string) {
 	sendEmail(message)
 	time.Sleep(time.Millisecond * 500)
 	fmt.Println("========================")
+}
+
+// Channels are a way to communicate between goroutines. They allow you to send and receive values of a specific type between different parts of your program. In this example, we will demonstrate how to use channels to send and receive messages concurrently.
+type email struct {
+	body string
+	date time.Time
+}
+
+func CheckEmailAge(emails [3]email) [3]bool {
+	isOldChan := make(chan bool)
+
+	go sendIsOld(isOldChan, emails)
+
+	isOld := [3]bool{}
+	isOld[0] = <-isOldChan
+	isOld[1] = <-isOldChan
+	isOld[2] = <-isOldChan
+	return isOld
+}
+
+// don't touch below this line
+
+func sendIsOld(isOldChan chan<- bool, emails [3]email) {
+	for _, e := range emails {
+		if e.date.Before(time.Date(2020, 0, 0, 0, 0, 0, 0, time.UTC)) {
+			isOldChan <- true
+			continue
+		}
+		isOldChan <- false
+	}
 }
---
+++ channels/channels_test.go
@@ -0,0 +1,66 @@
+package channels
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCheckEmailAge(t *testing.T) {
+	tests := []struct {
+		emails [3]email
+		isOld  [3]bool
+	}{
+		{[3]email{
+			{
+				body: "Words are pale shadows of forgotten names. As names have power, words have power.",
+				date: time.Date(2019, 2, 0, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				body: "It's like everyone tells a story about themselves inside their own head.",
+				date: time.Date(2021, 3, 1, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				body: "Bones mend. Regret stays with you forever.",
+				date: time.Date(2022, 1, 2, 0, 0, 0, 0, time.UTC),
+			},
+		}, [3]bool{true, false, false}},
+		{[3]email{
+			{
+				body: "Music is a proud, temperamental mistress.",
+				date: time.Date(2018, 0, 0, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				body: "Have you heard of that website Boot.dev?",
+				date: time.Date(2017, 0, 0, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				body: "It's awesome honestly.",
+				date: time.Date(2016, 0, 0, 0, 0, 0, 0, time.UTC),
+			},
+		}, [3]bool{true, true, true}},
+		{[3]email{
+			{
+				body: "I have stolen princesses back from sleeping barrow kings.",
+				date: time.Date(2019, 0, 0, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				body: "I burned down the town of Trebon",
+				date: time.Date(2019, 6, 6, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				body: "I have spent the night with Felurian and left with both my sanity and my life.",
+				date: time.Date(2022, 7, 0, 0, 0, 0, 0, time.UTC),
+			},
+		}, [3]bool{true, true, false}},
+	}
+	for _, tt := range tests {
+		got := CheckEmailAge(tt.emails)
+		fmt.Printf("CheckEmailAge(%v) = %v; want %v\n", tt.emails, got, tt.isOld)
+		for i := range got {
+			if got[i] != tt.isOld[i] {
+				t.Errorf("CheckEmailAge(%v) = %v; want %v", tt.emails, got, tt.isOld)
+			}
+		}
+	}
+}
