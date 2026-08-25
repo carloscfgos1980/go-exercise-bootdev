351d64674c79aa731b8e0e6460cbd61198badb7f
carlosinfante <kuva5008@gmail.com>
2026-08-25 17:10:35 +0200

    constrains

--- generics/generics.go
+++ generics/generics.go
@@ -1,9 +1,67 @@
 package generics
 
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+// getLast returns the last element of a slice of any type. If the slice is empty, it returns the zero value of the type.
 func getLast[T any](s []T) T {
 	if len(s) == 0 {
 		var zero T
 		return zero
 	}
 	return s[len(s)-1]
+}
+
+// Constrains. We have different kinds of "line items" that we charge our customer's credit cards for. Line items can be things like "subscriptions" or "one-time payments" for email usage.
+func ChargeForLineItem[T lineItem](newItem T, oldItems []T, balance float64) ([]T, float64, error) {
+	if balance < newItem.GetCost() {
+		return nil, 0.0, errors.New("insufficient funds")
+	}
+	oldItems = append(oldItems, newItem)
+	balance -= newItem.GetCost()
+	return oldItems, balance, nil
+}
+
+// don't edit below this line
+
+type lineItem interface {
+	GetCost() float64
+	GetName() string
+}
+
+type subscription struct {
+	userEmail string
+	startDate time.Time
+	interval  string
+}
+
+func (s subscription) GetName() string {
+	return fmt.Sprintf("%s subscription", s.interval)
+}
+
+func (s subscription) GetCost() float64 {
+	if s.interval == "monthly" {
+		return 25.00
+	}
+	if s.interval == "yearly" {
+		return 250.00
+	}
+	return 0.0
+}
+
+type oneTimeUsagePlan struct {
+	userEmail        string
+	numEmailsAllowed int
+}
+
+func (otup oneTimeUsagePlan) GetName() string {
+	return fmt.Sprintf("one time usage plan with %v emails", otup.numEmailsAllowed)
+}
+
+func (otup oneTimeUsagePlan) GetCost() float64 {
+	const costPerEmail = 0.03
+	return float64(otup.numEmailsAllowed) * costPerEmail
 }
--- generics/generics_test.go
+++ generics/generics_test.go
@@ -3,6 +3,7 @@ package generics
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestGetLast(t *testing.T) {
@@ -39,4 +40,99 @@ func TestGetLast(t *testing.T) {
 			t.Errorf("unsupported type: %T", input)
 		}
 	}
+}
+
+func TestChargeForLineItem(t *testing.T) {
+	tests := []struct {
+		newItem           lineItem
+		oldItems          []lineItem
+		balance           float64
+		expected          []lineItem
+		expectedBalance   float64
+		expectedErrString string
+	}{
+		{
+			newItem: subscription{
+				userEmail: "geralt@rivia.com",
+				startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+				interval:  "yearly",
+			},
+			oldItems: []lineItem{
+				subscription{
+					userEmail: "yen@vengerberg.com",
+					startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+					interval:  "monthly",
+				},
+				oneTimeUsagePlan{
+					userEmail:        "triss@maribor",
+					numEmailsAllowed: 100,
+				},
+			},
+			balance: 1000.00,
+			expected: []lineItem{
+				subscription{
+					userEmail: "yen@vengerberg.com",
+					startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+					interval:  "monthly",
+				},
+				oneTimeUsagePlan{
+					userEmail:        "triss@maribor",
+					numEmailsAllowed: 100,
+				},
+				subscription{
+					userEmail: "geralt@rivia.com",
+					startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+					interval:  "yearly",
+				},
+			},
+			expectedBalance:   750.00,
+			expectedErrString: "",
+		},
+		{
+			newItem: subscription{
+				userEmail: "geralt@rivia.com",
+				startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+				interval:  "yearly",
+			},
+			oldItems: []lineItem{
+				subscription{
+					userEmail: "yen@vengerberg.com",
+					startDate: time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+					interval:  "monthly",
+				},
+				oneTimeUsagePlan{
+					userEmail:        "triss@maribor",
+					numEmailsAllowed: 100,
+				},
+			},
+			balance:           200.00,
+			expected:          nil,
+			expectedBalance:   0.0,
+			expectedErrString: "insufficient funds",
+		},
+	}
+	for _, tt := range tests {
+		got, gotBalance, err := ChargeForLineItem(tt.newItem, tt.oldItems, tt.balance)
+		if err != nil && err.Error() != tt.expectedErrString {
+			t.Errorf("ChargeForLineItem() error = %v; want %v", err.Error(), tt.expectedErrString)
+		}
+		if !equalLineItems(got, tt.expected) {
+			t.Errorf("ChargeForLineItem() got = %v; want %v", got, tt.expected)
+		}
+		if gotBalance != tt.expectedBalance {
+			t.Errorf("ChargeForLineItem() gotBalance = %v; want %v", gotBalance, tt.expectedBalance)
+		}
+	}
+}
+
+func equalLineItems(a, b []lineItem) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i].GetName() != b[i].GetName() || a[i].GetCost() != b[i].GetCost() {
+			return false
+		}
+	}
+	return true
 }
